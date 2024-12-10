64d24779b441147d8040c0c85945bd87d682f0d0
4tpPostroznyDamian <4tpPostroznyDamian@S27K113>
2024-12-10 09:27:20 +0100

    Dodanie Mnozenia

---
+++ Kalkulator/Kalkulator_Mnoz.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulator
+{
+    public partial class Kalkulator
+    {
+        public int Mnoz(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
