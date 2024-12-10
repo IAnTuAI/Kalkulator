359ef4ab880ebee2ad61ead502716144f9173eec
4tpPostroznyDamian <4tpPostroznyDamian@S27K113>
2024-12-10 09:45:35 +0100

    Dzielenie

---
+++ Kalkulator/Kalkulator_Dzielenie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulator
+{
+    internal class Kalkulator_Dzielenie
+    {
+        public int Mnoz(int x, int y)
+        {
+            return x / y;
+        }
+    }
+}
