746c25d105dae9574c95f2d17417f7f8969aeb5c
awojtas <antoniwojtas2231+unperfect@gmail.com>
2024-12-10 09:42:00 +0100

    Repair, file not finding

--- Kalkulator/Program.cs
+++ Kalkulator/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace Kalkulator
+﻿using System;
+
+namespace Kalkulator
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Kalkulator kalkulator = new Kalkulator();
+            int a = kalkulator.Mnoz(1, 2);
+            int b = kalkulator.Dodaj(1, 2);
+            Console.WriteLine(a + " " + b);
         }
     }
 }
